42ba5a417c1dafdf54b687efa99508a4f8a1e3c4
Mitzingdash <wearwolfgamer99@gmail.com>
2024-02-11 14:54:33 +0100

    Updating the bss screen

--- src/main/java/food/food/client/gui/MitzFoodStatsTab.java
+++ src/main/java/food/food/client/gui/MitzFoodStatsTab.java
@@ -18,11 +18,16 @@ public class MitzFoodStatsTab extends StatsTab
         final var stats = initContext.getStatsProvider();
         
         //make some GUI here
-        final var label = new TLabelElement(5, 5, 100, 20, Text.literal("Hello world!"));
-        label.setTextColor(0xffff00); //hexadecimal for yellow
-        panel.addChild(label);
+        final var header = new TLabelElement(5, 5, 100, 20, Text.literal("Currently Under Development!"));
+        header.setTextScale(1.5f);
+        header.setTextColor(0xffff00); //hexadecimal for yellow
+        panel.addChild(header);
         
-        final var button = new TButtonWidget(5, 30, 100, 20, Text.literal("Click me!"));
+        final var extra = new TLabelElement(5, 30, 100, 20, Text.literal("Here.. Have a button"));
+        extra.setTextColor(0xffffff); //hexadecimal for yellow
+        panel.addChild(extra);
+        
+        final var button = new TButtonWidget(5, 50, 100, 20, Text.literal("Click me!"));
         button.setOnClick(btn -> btn.setText(Text.literal("Nice! You clicked on me!")));
         panel.addChild(button);
     }
